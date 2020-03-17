4b996d6b6f554ac4df4cf6893a0d1823f443bb76
ynque <yngve@kippersund.no>
2020-03-17 13:58:47 +0100

    uart_read works as intended. Toggling matrix is slightly wonky; some characters on the keyboard work differently than others. By and large works as intended.

--- 2_uart/main.c
+++ 2_uart/main.c
@@ -19,6 +19,41 @@ typedef struct {
 	volatile uint32_t PIN_CNF[32];
 } NRF_GPIO_REGS;
 
+void sendCharIfButton(){
+	if (((GPIO->IN)&(1 << __BUTTON_B_PIN__))==0x0){
+		uart_send('B');
+	}
+
+	if (((GPIO->IN)&(1 << __BUTTON_A_PIN__))==0x0){
+		uart_send('A');
+	}
+}
+
+int matrixToggled;
+int toggleMatrixIfInput(int toggled){
+	int slep = 10000;
+	if (uart_read() != '\0'){
+		if(toggled == 0){
+			for (int i = 13; i <= 15; i++){
+				GPIO->OUTSET = (1 << i);
+			}
+			toggled = 1;
+			while(!slep){
+				slep--;
+			}
+		}
+		else if(toggled == 1){
+			for (int i = 13; i <= 15; i++){
+				GPIO->OUTCLR = (1 << i);
+			}
+			toggled = 0;
+			while(!slep){
+				slep--;
+			}
+		}
+	}
+	return toggled;
+}
 int main(){
 
 	uart_init();
@@ -33,7 +68,8 @@ int main(){
 	GPIO->PIN_CNF[__BUTTON_B_PIN__] = 0;
 
 	int sleep = 0;
-
+	matrixToggled = 0;
+	
 		//Set to ground
 	for (int i = 4; i <= 12; i++){
 		GPIO->OUTCLR = (1 << i);
@@ -41,13 +77,9 @@ int main(){
 
 	while(1){
 
-		if (((GPIO->IN)&(1 << __BUTTON_B_PIN__))==0x0){
-			uart_send('B');
-		}
+		sendCharIfButton();
 
-		if (((GPIO->IN)&(1 << __BUTTON_A_PIN__))==0x0){
-			uart_send('A');
-		}
+		matrixToggled = toggleMatrixIfInput(matrixToggled); //Doesn't work
 
 		sleep = 10000;
 		while(--sleep);
